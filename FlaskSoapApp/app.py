848314534d65d201ce5859f6315861cde32ad175
OSCAR AGUILERA <oscar.aguileradlt12@gmail.com>
2024-09-23 01:28:10 -0600

    SOAP APP

---
+++ FlaskSoapApp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+from zeep import Client
+
+app = Flask(__name__)
+
+# API REST
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({'message': 'Hola desde la API REST, Oscar Aguilera!'})
+
+# Llamada SOAP
+@app.route('/api/soap', methods=['GET'])
+def call_soap():
+    url = 'http://www.dneonline.com/calculator.asmx?WSDL'  # Ejemplo de WSDL
+    client = Client(url)
+    result = client.service.Add(5, 3)  # Llama a la operación 'Add'
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
